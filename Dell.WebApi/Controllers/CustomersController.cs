275060a47c575477a46d5f45958ac8c185f454e8
Vlad Burghelea <45212038+burgheleavlad94@users.noreply.github.com>
2018-11-21 17:34:41 +0200

    Add files via upload

---
+++ Dell.WebApi/Controllers/CustomersController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dell.Data.Entities;
+using Dell.Data.EntityDB;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dell.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomersController : Controller
+    {
+        public CustomersDB CustomersRepo { get; set; }
+
+        public CustomersController(Data.EntityDB.CustomersDB customersDB )
+        {
+            CustomersRepo = customersDB;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IActionResult result = null;
+            try
+            {
+                var rez = CustomersRepo.ReadAll();
+
+                if (rez == null)
+                    result = new NotFoundResult();
+                else
+                    result = new OkObjectResult(rez);
+            }
+            catch (Exception ex)
+            {
+                result = new BadRequestObjectResult(ex);
+            }
+
+            return result;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult GetbyId(int id)
+        {
+            IActionResult result = null;
+            try
+            {
+                var rez = CustomersRepo.ReadByID(id);
+
+                if (rez == null)
+                    result = new NotFoundResult();
+                else
+                    result = new OkObjectResult(rez);
+            }
+            catch (Exception ex)
+            {
+                result = new BadRequestObjectResult(ex);
+            }
+
+            return result;
+        }
+
+        [HttpGet("byEmail/{email}")]
+        public IActionResult GetbyEmail(string email)
+        {
+            IActionResult result = null;
+            try
+            {
+                var rez = CustomersRepo.ReadByEmail(email);
+
+                if (rez == null)
+                    result = new NotFoundResult();
+                else
+                    result = new OkObjectResult(rez);
+            }
+            catch (Exception ex)
+            {
+                result = new BadRequestObjectResult(ex);
+            }
+
+            return result;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]Data.Entities.Customer entity)
+        {
+            IActionResult result = null;
+
+            if (entity != null)
+            {
+                try
+                {
+                    var rez = CustomersRepo.Insert(entity);
+
+                    if (rez <= 0) 
+                        result = new BadRequestResult();
+                    else
+                    {
+                        var add = CustomersRepo.ReadByID(entity.ID);
+                        result = new OkObjectResult(add);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    result = new BadRequestObjectResult(ex);
+                }
+            }
+            return result;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Customer entity)
+        {
+            IActionResult result = null;
+
+            if (entity != null)
+            {
+                try
+                {
+                    var rez = CustomersRepo.Update(entity);
+
+                    if (rez <= 0)
+                        result = new BadRequestResult();
+                    else
+                    {
+                        var add = CustomersRepo.ReadByID(entity.ID);
+                        result = new OkObjectResult(add);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    result = new BadRequestObjectResult(ex);
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Dell.WebApi/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Dell.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dell.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var config = Configuration.GetSection("Config").Get<Config>();
+
+            services.AddMvc();
+
+            services.AddScoped(x => new Data.EntityDB.CustomersDB(config.Connections.GetDefaultConnection().ConnectionString));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                );
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
