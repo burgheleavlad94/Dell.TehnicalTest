449b118514059eaf6988bd0a3a15159b7f06b784
Vlad Burghelea <45212038+burgheleavlad94@users.noreply.github.com>
2018-11-21 18:25:00 +0200

    Add files via upload

---
+++ Dell.WPF/bin/Debug/Update.xaml.cs
@@ -0,0 +1,108 @@
+﻿using Dell.Data.Entities;
+using System;
+using System.Windows;
+using System.Linq;
+using System.Windows.Input;
+using System.Text.RegularExpressions;
+using Dell.WebAPICall;
+using System.Windows.Controls;
+
+namespace Dell.WPF
+{
+    /// <summary>
+    /// Interaction logic for Update.xaml
+    /// </summary>
+    public partial class Update : Window
+    {
+        
+        public Customer customer = null;
+        bool isNew = false;
+
+        public Update()
+        {
+            InitializeComponent();
+            Loaded += CustomersUpdate_Loaded;
+        }
+
+        private void CustomersUpdate_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                name.Focus();
+                if (customer == null)
+                {
+                    isNew = true;
+                    customer = new Customer();
+                }
+                this.Title = (isNew ? "Insert" : "Update");
+                this.DataContext = customer;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Reading customer for {(isNew ? "insert" : "update") } failed!!!", ex.Message);
+            }
+        }
+
+        private async void Update_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (customer.Errors > 0)
+                    MessageBox.Show("Record has errors!");
+                else
+                {
+                    WebAPI webAPI = new WebAPI();
+                
+                    var customers = await webAPI.ReadByEmail(customer.EmailAddress);
+                
+                    bool sameEmail = false;
+
+                    if (customers?.Count > 0)
+                    {
+                        var cust = from x in customers where x.EmailAddress == customer.EmailAddress && (isNew ? true : x.ID != customer.ID) select x;
+                        sameEmail = cust?.Count() > 0;
+                    }
+
+                    if (sameEmail)
+                        MessageBox.Show("Another customer is registered with same email!");
+                    else
+                    {
+                    
+                        var result = this.DialogResult = await (isNew ? webAPI.Add(customer) : webAPI.Update(customer));
+                        if (result.HasValue && result.Value)
+                            MessageBox.Show($"{(isNew? "Insert" : "Update")} succeded!");
+                        else
+                            MessageBox.Show($"{(isNew ? "Insert" : "Update") } failed!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{(isNew ? "Insert" : "Update")} failed! {Environment.NewLine}{ex.Message}");
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void EmailValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void Validation_Error(object sender, ValidationErrorEventArgs e)
+        {
+            if (e.Action == ValidationErrorEventAction.Added) customer.Errors += 1;
+            if (e.Action == ValidationErrorEventAction.Removed) customer.Errors -= 1;
+        }
+    }
+}
---
+++ Dell.WPF/bin/Debug/WebApi.Call.cs
@@ -0,0 +1,134 @@
+﻿using Dell.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dell.WebAPICall
+{ 
+    public class WebAPI
+    {
+        /// <summary>
+        /// Create a http client
+        /// </summary>
+        /// <returns>a <see cref="HttpClient"/></returns>
+        public static HttpClient GetHttpClient()
+        {
+            HttpClient client = new HttpClient()
+            {
+                BaseAddress = new Uri("http://localhost:49911/api/")
+            };
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json") { CharSet = "utf-8" });
+
+            return client;
+        }
+
+        /// <summary>
+        /// Fetch a list with <see cref="Customer"/> objects from database.
+        /// </summary>
+        /// <returns>a <see cref="List{Customer}"/>.</returns>
+        public async Task<IList<Customer>> Read()
+        {
+            IList<Customer> customers = null;
+            try
+            {
+                using (var client = GetHttpClient())
+                {
+                    var response = await client.GetAsync("customers");
+                    response.EnsureSuccessStatusCode();
+                    customers = await response.Content.ReadAsAsync<IList<Customer>>();
+                }
+                return customers;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Read failed!", ex);
+            }
+        }
+
+        /// <summary>
+        /// Fetch a <see cref="Customer"/> object of the specific id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>a <see cref="Customer"/></returns>
+        public async Task<Customer> ReadByID(int id)
+        {
+            Customer customer = null;
+            try
+            {
+                using (var client = GetHttpClient())
+                {
+                    var response = await client.GetAsync("customers/" + id);
+                    response.EnsureSuccessStatusCode();
+                    customer = await response.Content.ReadAsAsync<Customer>();
+                }
+                return customer;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Read failed!", ex);
+            }
+        }
+
+        /// <summary>
+        /// Fetch a list with <see cref="Customer"/> objects from database, with a specific e-mail
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>a <see cref="List{Customer}"/></returns>
+        public async Task<List<Customer>> ReadByEmail(string email)
+        {
+            List<Customer> customer = null;
+         
+            using (var client = GetHttpClient())
+            {
+                var response = await client.GetAsync("customers/byEmail/" + email);
+                customer = await response.Content.ReadAsAsync<List<Customer>>();
+            }
+            return customer;
+        }
+
+        /// <summary>
+        /// Add a new customer in database
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns> a <see cref="bool"/></returns>
+        public async Task<bool> Add(Customer customer)
+        {
+            bool result = false;
+            try
+            {
+                using (var httpClient = GetHttpClient())
+                {
+                    var response = await httpClient.PostAsJsonAsync("customers/", customer);
+                    response.EnsureSuccessStatusCode();
+                    result = true;
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                result = false;
+                throw new Exception("Inserted failed!", ex);
+            }
+        }
+
+        /// <summary>
+        /// Update a customer in databse
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns>a <see cref="bool"/></returns>
+        public async Task<bool> Update(Customer customer)
+        {
+            bool result = true;
+            using (var client = GetHttpClient())
+            {
+                var response = await client.PutAsJsonAsync($"customers/{customer.ID}", customer);
+                response.EnsureSuccessStatusCode();
+            }
+            return result;
+        }
+    }
+}
