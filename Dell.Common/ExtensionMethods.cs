8557d32caf0208cf44cf68196b6c491678ce4c82
Vlad Burghelea <45212038+burgheleavlad94@users.noreply.github.com>
2018-11-21 17:21:41 +0200

    Add files via upload

---
+++ Dell.Common/ExtensionMethods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+
+namespace Dell.Common
+{
+    public static class ExtensionMethods
+    {
+        public static void OpenEx(this DbConnection connection)
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+        }
+
+        public static void CloseEx(this DbConnection connection)
+        {
+            if (connection != null && connection.State != System.Data.ConnectionState.Closed)
+                connection.Close();
+        }
+
+        public static void CloseEx(this DbDataReader reader)
+        {
+            if (reader != null && !reader.IsClosed)
+                reader.Close();
+        }
+
+        public static int GetParamReturnValueEx(this DbCommand command)
+        {
+            int returnVal = 0;
+            //var retVal = command.Parameters?.Cast<DbParameter>().Where(p => p.ParameterName == "@retVal").FirstOrDefault();
+            //if (retVal != null && retVal.Value != DBNull.Value)
+            //    returnVal = (int)retVal.Value;
+
+            var tmp = command.Parameters?.Cast<DbParameter>().FirstOrDefault(p => p.Direction == ParameterDirection.ReturnValue)?.Value;
+            if (tmp != null && tmp != DBNull.Value)
+                returnVal = (int)tmp;
+            return returnVal;
+
+        }
+    }
+}
